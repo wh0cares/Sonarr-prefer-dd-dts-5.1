4d90c7689ca133aa91bb597dadae2e2956871897
Denis Suarez <babydenis8@gmail.com>
2018-11-10 02:37:03 -0500

    Get episodes in Sonarr and get a DD, 5.1, or a DTS release for episodes with AAC audio

---
+++ api.py
@@ -0,0 +1,22 @@
+import requests
+
+class Sonarr(object):
+    def __init__(self, url, api_key):
+        self.url = url
+        self.api_key = api_key
+
+    def get_series(self):
+        response = requests.get("{}/api/series?apikey={}".format(self.url, self.api_key))
+        return response.json()
+
+    def get_episodes(self, series_id):
+        response = requests.get("{}/api/episode?seriesId={}&apikey={}".format(self.url, series_id, self.api_key))
+        return response.json()
+
+    def get_release(self, episode_id):
+        response = requests.get("{}/api/release?episodeId={}&apikey={}".format(self.url, episode_id, self.api_key))
+        return response.json()
+
+    def post_release(self, guid, indexer_id):
+        response = requests.post("{}/api/release?guid={}&indexerId={}&apikey={}".format(self.url, guid, indexer_id, self.api_key))
+        return response.json()
---
+++ run.py
@@ -0,0 +1,25 @@
+import json
+from api import Sonarr
+
+with open('config.json') as file:
+    data = json.load(file)
+    url = data['url']
+    api_key = data['api_key']
+
+sonarr = Sonarr(url, api_key)
+
+for series in sonarr.get_series():
+    series_id = series['id']
+    for episode in sonarr.get_episodes(series_id):
+        if episode['hasFile']:
+            if episode['episodeFile']['mediaInfo']['audioCodec'] == "AAC":
+                episode_id = episode['id']
+                for releases in sonarr.get_release(episode_id):
+                    if any(i in releases['title'].upper() for i in ['DD', '5.1', 'DTS']):
+                        guid = releases['guid']
+                        indexer_id = releases['indexerId']
+                        sonarr.post_release(guid, indexer_id)
+                        print releases['title'].encode('utf-8')
+                        break
+
+print "done"
