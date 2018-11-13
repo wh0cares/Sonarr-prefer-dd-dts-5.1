135e4e2ea44204b661d456fd54d90d208d75cadc
Denis Suarez <babydenis8@gmail.com>
2018-11-13 18:43:49 -0500

    Add progress bar with messages & requirements.txt

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import json
+from tqdm import tqdm
 from api import Sonarr
 
 with open('config.json') as file:
@@ -6,14 +8,16 @@ with open('config.json') as file:
     url = data['url']
     api_key = data['api_key']
 
+print 'Searching series...'
 sonarr = Sonarr(url, api_key)
-
-for series in sonarr.get_series():
+for series in tqdm(sonarr.get_series()):
+    tqdm.write('Searching ' + series['title'] + ' episodes')
     series_id = series['id']
     for episode in sonarr.get_episodes(series_id):
         if episode['hasFile']:
             if episode['episodeFile']['mediaInfo']['audioCodec'] == "AAC":
                 episode_id = episode['id']
+                tqdm.write('Searching ' + series['title'] + " " + str(episode['seasonNumber']) + 'x' + str(episode['episodeNumber'])  + ' releases')
                 for releases in sonarr.get_release(episode_id):
                     if any(i in releases['title'].upper() for i in ['DD', '5.1', 'DTS']):
                         guid = releases['guid']
@@ -34,7 +38,7 @@ for series in sonarr.get_series():
                         if(rejected):
                             continue
                         else:
-                            print releases['title'].encode('utf-8')
+                            tqdm.write('Grabbing ' + releases['title'])
                             sonarr.post_release(guid, indexer_id)
                             break
 
